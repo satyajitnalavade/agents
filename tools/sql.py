efd3c6f5a5e3dc5b5cce91a50951d84db9683b23
satyajitn <satyajit.nalavade@gmail.com>
2024-01-07 20:08:29 -0600

    Improve argument naming using customizations

--- tools/sql.py
+++ tools/sql.py
@@ -1,5 +1,7 @@
 import sqlite3
 from langchain.tools import Tool
+from pydantic.v1 import BaseModel
+from typing import List
 
 conn = sqlite3.connect("db.sqlite")
 
@@ -17,11 +19,15 @@ def run_sqlite_query(query):
    except sqlite3.OperationalError as err:
        return f"The following error occured: {str(err)}"
 
+class RunQueryArgsSchema(BaseModel):
+    query: str
 
 run_query_tool = Tool.from_function(
     name= "run_sqlite_query",
     description="Run a sqlite query.",
-    func=run_sqlite_query
+    func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema
+    
 ) 
 
 def describe_tables(table_names):
@@ -30,8 +36,12 @@ def describe_tables(table_names):
     rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' and name IN ({tables});")
     return '\n'.join(row[0] for row in rows if row[0] is not None)
 
+class DescribeTablesArgsSchema(BaseModel):
+    table_name:str
+
 describe_tables_tool = Tool.from_function(
     name="describe_tables",
     description="Given a list of table names, return the schema of those tables",
-    func=describe_tables
+    func=describe_tables,
+    args_schema=DescribeTablesArgsSchema
 )
